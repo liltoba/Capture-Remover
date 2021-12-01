189253ebcb24c7b7bb3f3dc78ee587b4a7d8125f
liltoba <59983513+liltoba@users.noreply.github.com>
2021-12-01 17:13:33 +0300

    Add files via upload

---
+++ CaptureRemover/CaptureRemover/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace CaptureRemover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.path = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.OwnerForm = this;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton1.Highlight = false;
+            this.metroButton1.Location = new System.Drawing.Point(23, 206);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(507, 40);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroButton1.StyleManager = this.metroStyleManager1;
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Start";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroComboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "Username:Password",
+            "Mail:Password"});
+            this.metroComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.metroComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.metroComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.metroComboBox1.FontSize = MetroFramework.MetroLinkSize.Medium;
+            this.metroComboBox1.FontWeight = MetroFramework.MetroLinkWeight.Regular;
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Username:Password",
+            "Mail:Password"});
+            this.metroComboBox1.Location = new System.Drawing.Point(23, 171);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(507, 29);
+            this.metroComboBox1.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroComboBox1.StyleManager = this.metroStyleManager1;
+            this.metroComboBox1.TabIndex = 1;
+            this.metroComboBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // path
+            // 
+            this.path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.path.FontSize = MetroFramework.MetroTextBoxSize.Small;
+            this.path.FontWeight = MetroFramework.MetroTextBoxWeight.Regular;
+            this.path.Location = new System.Drawing.Point(23, 106);
+            this.path.Multiline = false;
+            this.path.Name = "path";
+            this.path.SelectedText = "";
+            this.path.Size = new System.Drawing.Size(470, 23);
+            this.path.Style = MetroFramework.MetroColorStyle.Pink;
+            this.path.StyleManager = this.metroStyleManager1;
+            this.path.TabIndex = 2;
+            this.path.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.path.UseStyleColors = false;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton2.Highlight = false;
+            this.metroButton2.Location = new System.Drawing.Point(499, 106);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(31, 23);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroButton2.StyleManager = this.metroStyleManager1;
+            this.metroButton2.TabIndex = 8;
+            this.metroButton2.Text = "...";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.CustomBackground = false;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Medium;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.metroLabel1.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 75);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(75, 19);
+            this.metroLabel1.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroLabel1.StyleManager = this.metroStyleManager1;
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Select Path:";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel1.UseStyleColors = false;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.CustomBackground = false;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Medium;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.metroLabel2.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 140);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(39, 19);
+            this.metroLabel2.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroLabel2.StyleManager = this.metroStyleManager1;
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "Type:";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel2.UseStyleColors = false;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.CustomBackground = false;
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Medium;
+            this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.metroLabel3.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.metroLabel3.Location = new System.Drawing.Point(299, 50);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(144, 19);
+            this.metroLabel3.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroLabel3.StyleManager = this.metroStyleManager1;
+            this.metroLabel3.TabIndex = 9;
+            this.metroLabel3.Text = "https://t.me/ghoulblack";
+            this.metroLabel3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel3.UseStyleColors = false;
+            this.metroLabel3.Click += new System.EventHandler(this.metroLabel3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 261);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Pink;
+            this.StyleManager = this.metroStyleManager1;
+            this.Text = "Capture Remover | Coded By LilToba";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroTextBox path;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+    }
+}
+
---
+++ CaptureRemover/CaptureRemover/Form1.cs
@@ -0,0 +1,130 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureRemover
+{
+    public partial class Form1 : MetroForm
+    {
+        class Data
+        {
+            public string Login { get; set; }
+            public string Domen { get; set; }
+            public string Pass { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+            if (metroComboBox1.SelectedItem == "Mail:Password")
+            {
+
+                string data = File.ReadAllText(path.Text);
+                                                         
+                Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*:\w+([-+.]\w+)*",
+                    RegexOptions.IgnoreCase);
+          
+                MatchCollection emailMatches = emailRegex.Matches(data);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (Match emailMatch in emailMatches)
+                {
+                    sb.AppendLine(emailMatch.Value);
+                }
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Title = "Save Combo Files";
+                saveFileDialog1.DefaultExt = "txt";
+                saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    var sw = new StreamWriter(saveFileDialog1.FileName);
+                    sw.WriteLine(sb.ToString());
+                    sw.Close();
+                }
+
+            }
+            if (metroComboBox1.SelectedItem == "Username:Password")
+            {
+                string data = File.ReadAllText(path.Text); 
+                                                          
+                Regex emailRegex = new Regex(@"\w+([-+.]\w+)*:\w+([-+.]\w+)*",
+                    RegexOptions.IgnoreCase);
+ 
+                MatchCollection emailMatches = emailRegex.Matches(data);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (Match emailMatch in emailMatches)
+                {
+                    sb.AppendLine(emailMatch.Value);
+                }
+            
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();   
+    saveFileDialog1.Title = "Save Combo Files";
+                saveFileDialog1.DefaultExt = "txt";
+                saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                     var sw = new StreamWriter(saveFileDialog1.FileName);
+                    sw.WriteLine(sb.ToString());
+                    sw.Close();
+
+                }
+
+   
+
+            }
+            else
+            {
+
+            }
+
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+     
+                Title = "Browse Combolist Files",
+
+        
+                Filter = "Combolist files (*.txt)|*.txt",
+            
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://t.me/ghoulblack");
+        }
+    }
+
+
+
+}
